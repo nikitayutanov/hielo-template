c64e30d0122b6490c666d21afbcdfab819d76c67
Nikita Yutanov <nikitayutanov@gmail.com>
2020-08-12 17:56:24 +0500

    Add oustide click toggle for menu

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 'use strict';
 
-const menuButton = document.querySelector('.menu__button');
-const hamburger = document.querySelector('.menu__hamburger');
-const menu = document.querySelector('.menu__list');
+const menu = {
+    nav: document.querySelector('.menu'),
+    visible: false,
 
-let menuVisible = false;
+    open() {
+        this.nav.classList.add('menu--open');
+        this.visible = true;
+    },
 
-menuButton.addEventListener('click', toggleMenu);
-
-function toggleMenu() {
-    if (!menuVisible) {
-        menu.classList.add('menu__list--open');
-        hamburger.classList.add('menu__hamburger--open');
-        menuVisible = true;
-    } else {
-        menu.classList.remove('menu__list--open');
-        hamburger.classList.remove('menu__hamburger--open');
-        menuVisible = false;
+    close() {
+        this.nav.classList.remove('menu--open');
+        this.visible = false;
     }
 }
+
+document.addEventListener('click', () => {
+    if (event.target.closest('.menu__button') && !menu.visible) {
+        menu.open();
+    } else if (!event.target.closest('.menu__list') && menu.visible) {
+        menu.close();
+    }
+});
