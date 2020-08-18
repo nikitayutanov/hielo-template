ef58abebad4c9834f1ecb386c4034a8b529cc754
Nikita Yutanov <nikitayutanov@gmail.com>
2020-08-19 02:36:01 +0500

    Add event listeners delegation

--- script.js
+++ script.js
@@ -3,60 +3,76 @@
 // Menu
 
 const menu = {
+    class: {
+        button: '.menu__button',
+        list: '.menu__list',
+        open: 'menu--open'
+    },
+
     nav: document.querySelector('.menu'),
     visible: false,
 
     open() {
-        this.nav.classList.add('menu--open');
+        this.nav.classList.add(this.class.open);
         this.visible = true;
     },
 
     close() {
-        this.nav.classList.remove('menu--open');
+        this.nav.classList.remove(this.class.open);
         this.visible = false;
+    },
+
+    toggle(e) { 
+        if (e.target.closest(this.class.button) && !this.visible) {
+            this.open();
+        } else if (!e.target.closest(this.class.list) && this.visible) {
+            this.close();
+        }
     }
 }
 
-document.addEventListener('click', (event) => {
-    if (event.target.closest('.menu__button') && !menu.visible) {
-        menu.open();
-    } else if (!event.target.closest('.menu__list') && menu.visible) {
-        menu.close();
-    }
-});
+menu.nav.addEventListener('click', (e) => menu.toggle(e));
 
 // Slider
 
-const prevBtn = document.querySelector('.slider__side-button--prev');
-const nextBtn = document.querySelector('.slider__side-button--next');
-const buttons = document.querySelectorAll('.slider__button');
-const startPosition = 0;
+const slider = document.querySelector('.slider');
+const btnClass = '.slider__button';
+const buttons = document.querySelectorAll(btnClass);
+const prevBtnClass = '.slider__side-button--prev';
+const nextBtnClass = '.slider__side-button--next';
+const firstSlide = 0;
 const lastSlide = buttons.length - 1;
-let position = startPosition;
-
+let currentSlide = firstSlide;
 
 function nextSlide() {
-    if (position != lastSlide) {
-        position++;
+    if (currentSlide != lastSlide) {
+        currentSlide++;
     } else {
-        position = startPosition;
+        currentSlide = firstSlide;
     }
-    buttons[position].checked = true;
+    buttons[currentSlide].checked = true;
 }
 
 function prevSlide() {
-    if (position != startPosition) {
-        position--;
+    if (currentSlide != firstSlide) {
+        currentSlide--;
     } else {
-        position = lastSlide;
+        currentSlide = lastSlide;
     }
-    buttons[position].checked = true;
+    buttons[currentSlide].checked = true;
 }
 
-prevBtn.addEventListener('click', () => prevSlide());
-nextBtn.addEventListener('click', () => nextSlide());
+function changeSlide(e) {
+    if (e.target.closest(prevBtnClass)) {
+        prevSlide();
+    } else if (e.target.closest(nextBtnClass)) {
+        nextSlide();
+    } else if (e.target.closest(btnClass)) {
+        currentSlide = e.target.closest(btnClass).dataset.index;
+    }
+}
 
-buttons.forEach((slide, index) => slide.addEventListener('click', () => position = index));
+slider.addEventListener('click', (e) => changeSlide(e));
 
 setInterval(nextSlide, 5000);
 
@@ -73,11 +89,15 @@ window.addEventListener('scroll', () => {
 
 // Gallery
 
-const images = document.querySelectorAll('.gallery__image');
-const modalWindow = document.querySelector('.modal');
+const gallery = document.querySelector('.gallery__main');
+const modal = document.querySelector('.modal');
 const modalImage = document.querySelector('.modal__image');
-const prevBtnImg = document.querySelector('.modal__side-button--prev');
-const nextBtnImg = document.querySelector('.modal__side-button--next');
+const imgClass = '.gallery__image';
+const images = document.querySelectorAll(imgClass);
+const prevImgBtnClass = '.modal__side-button--prev';
+const nextImgBtnClass = '.modal__side-button--next';
+const crossBtnClass = '.modal__cross-button';
+const modalVisibleClass = 'modal--visible';
 let firstImage = 0;
 let lastImage = images.length - 1;
 let currentImage;
@@ -102,19 +122,30 @@ function prevImage() {
     modalImage.src = images[currentImage].src;
 }
 
-images.forEach((image, index) => {
-    image.addEventListener('click', () => {
-        currentImage = index;
-        modalImage.src = image.src;
-        modalWindow.classList.add('modal--visible');
-    });
-});
-
-prevBtnImg.addEventListener('click', () => prevImage());
-nextBtnImg.addEventListener('click', () => nextImage());
-
-modalWindow.addEventListener('click', (event) => {
-    if (event.target == modalWindow || event.target.closest('.modal__cross-button')) {
-        modalWindow.classList.remove('modal--visible');
+function showPopup(e) {
+    if (e.target.closest(imgClass)) {
+        currentImage = e.target.closest(imgClass).dataset.index;
+        modalImage.src = e.target.closest(imgClass).src;
+        modal.classList.add(modalVisibleClass);
     }
+}
+
+function closePopup(e) {
+    if (e.target == modal || e.target.closest(crossBtnClass)) {
+        modal.classList.remove(modalVisibleClass);
+    }
+}
+
+function changeImage(e) {
+    if (e.target.closest(prevImgBtnClass)) {
+        prevImage();
+    } else if (e.target.closest(nextImgBtnClass)) {
+        nextImage();
+    }
+}
+
+gallery.addEventListener('click', (e) => {
+    showPopup(e);
+    closePopup(e);
+    changeImage(e);
 });
